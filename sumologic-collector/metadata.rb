e1b92a3c3d5fb8f5c778e87bb77e4a4c3cb60109
Ben Newton <ben.newton@yahoo.com>
2013-05-27 21:37:59 -0700

    First Commit

--- sumologic-collector/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'sumologic-collector'
-maintainer       'Ben Newton'
-maintainer_email 'ben@sumologic.com'
-license          'Apache v2.0'
-description      'Installs/Configures sumologic-collector'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.01'
