200d5365ad5b5602dadc558da4af28f3d65a775f
Yoway Buorn <yowayb@gmail.com>
2014-02-25 21:04:18 -0800

    ignore generated metadata.json files

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,14 @@
-name             'sumo-collector-chef-cookbook'
 maintainer       'Ben Newton'
 maintainer_email 'ben@sumologic.com'
 license          'Apache v2.0'
 description      'Installs/Configures sumologic-collector'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '1.2.00'
+attribute 'accessId',
+  :display_name => "Access ID",
+  :type => "string",
+  :required => "required"
+attribute 'accessKey',
+  :display_name => "Access Key",
+  :type => "string",
+  :required => "required"
