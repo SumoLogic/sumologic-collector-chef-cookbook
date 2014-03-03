d3b67dc36a0eb2399827e68a7a96005c17d8ebb4
sumoway <yoway@sumologic.com>
2014-03-03 11:25:01 -0800

    updated version in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ben@sumologic.com'
 license          'Apache v2.0'
 description      'Installs/Configures sumologic-collector'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.0'
+version          '1.2.1'
 attribute 'sumologic/credentials/bag_name',
   :display_name => "Credentials bag name",
   :type => "string",
