3494e0e9269a1cc2434fcb71edf5d91fb69867a2
Brian Oldfield <brian.oldfield@socrata.com>
2014-09-10 11:22:19 -0700

    Enable installation in environments which require a proxy
    This update is restricted to non-windows environments only. It utilizes the
    collector tarball available for download via
    
    https://collectors.sumologic.com/rest/download/tar
    
    and replaces the bundled wrapper.conf with a template that utilizes the
    system java package and the supplied proxy settings.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ben@sumologic.com'
 license          'Apache v2.0'
 description      'Installs/Configures sumologic-collector'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.2.1'
+version          '1.2.2'
 attribute 'sumologic/credentials/bag_name',
   :display_name => "Credentials bag name",
   :type => "string",
@@ -13,3 +13,5 @@ attribute 'sumologic/credentials/item_name',
   :display_name => "Credentials item name",
   :type => "string",
   :required => "required"
+
+depends 'java'
