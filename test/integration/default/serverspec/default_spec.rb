6453f6a7a2ce72aa3ba293e72b3a832b2d8ac016
Michael Cizmar <michael.cizmar@mcplusa.com>
2015-09-28 10:31:05 -0500

    Started service tests

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,5 +1,8 @@
-require_relative 'spec_helper'
+require 'spec_helper'
+require 'sumo_helper'
 
+access_id = "suapsWeGL8iczv"
+access_key = "8Y2WyLOuwbkKtrQgUQTO3hSodMMBfyLPfw4gxYuhzvcU7yPFUIEXG34MI5FIAbJH"
 
 describe file('/etc/sumo.conf') do
    it { should exist }
@@ -16,4 +19,16 @@ end
 describe service('collector') do
   it { should be_running }
   it { should be_enabled }
+end
+
+describe host('service.sumologic.com') do
+  it { should be_reachable.with( :port => 443, :proto => 'tcp' ) }
+end
+
+describe "sumologic" do
+  it "should receive data" do
+    collector = Sumologic::Collector.new({ name: 'pd', api_username: access_id, api_password: access_key })
+    response = collector.search('vagrant')
+    expect(!response[0]['_raw'].nil?)
+  end
 end
