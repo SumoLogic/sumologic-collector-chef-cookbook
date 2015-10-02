ed397515e18ba81e72dd0f54d671f359a6ba7014
Michael Cizmar <michael.cizmar@mcplusa.com>
2015-10-01 23:31:06 -0500

    corrected some specs for OS dependencies

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -14,9 +14,13 @@ describe file('/opt/SumoCollector') do
   it { should be_directory }
 end
 
-describe service('collector') do
+describe service('collector'), :if => os[:family] == 'ubuntu' do
   it { should be_running }
   it { should be_enabled }
+end
+
+describe host('service.sumologic.com'), :if => os[:family] == 'ubuntu' do
+  it { should be_reachable.with( :port => 443, :proto => 'tcp' ) }
 end
 
 describe file ('/etc/sumo.conf') do
