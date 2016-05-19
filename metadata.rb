SumoLogic/sumologic-collector-chef-cookbook

commit ce631d9bae0ba6faac460dd75f5a52860f6e0f13

foodcritic - FC001 FC019 FC064 FC065

metadata.rb
@@ -1,6 +1,8 @@
 name 'sumologic-collector'
-maintainer 'Ben Newton'
-maintainer_email 'ben@sumologic.com'
+maintainer 'Yoway Buorn'
+maintainer_email 'yoway@sumologic.com'
+issues_url 'https://github.com/SumoLogic/sumologic-collector-chef-cookbook/issues'
+source_url 'https://github.com/SumoLogic/sumologic-collector-chef-cookbook'
 license 'Apache v2.0'
 description 'Installs/Configures sumologic-collector'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))

recipes/debian.rb
@@ -7,7 +7,7 @@ dpkg_package 'sumocollector' do
   action :install
 end
 
-if node[:platform] == 'debian' && Gem::Version.new(node[:platform_version]) >= Gem::Version.new('8.0')
+if node['platform'] == 'debian' && Gem::Version.new(node['platform_version']) >= Gem::Version.new('8.0')
 	execute 'sumo-systemd-reload' do
 	  command 'sudo /bin/systemctl --system daemon-reload && sudo systemctl restart collector.service'
 	  action :run

commit 66683167c4ad4785d4d327d2f0f16a3d3f30fd12

Adds guards for Chef 11 metadata compatibility.
Broken by ce631d9bae0ba6faac460dd75f5a52860f6e0f13.

metadata.rb
@@ -1,8 +1,8 @@
 name 'sumologic-collector'
 maintainer 'Yoway Buorn'
 maintainer_email 'yoway@sumologic.com'
-issues_url 'https://github.com/SumoLogic/sumologic-collector-chef-cookbook/issues'
-source_url 'https://github.com/SumoLogic/sumologic-collector-chef-cookbook'
+issues_url 'https://github.com/SumoLogic/sumologic-collector-chef-cookbook/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/SumoLogic/sumologic-collector-chef-cookbook' if respond_to?(:source_url)
 license 'Apache v2.0'
 description 'Installs/Configures sumologic-collector'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))