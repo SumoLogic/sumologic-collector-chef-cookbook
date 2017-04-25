362c4cbe2a99c772b96ce935e51cccc1bd773c6c
Ben Vidulich <ben@vidulich.co.nz>
2017-04-25 12:15:48 +1200

    License should use SPDX short identifer
    Recommendation as per FC069.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer 'Sumo Logic'
 maintainer_email 'opensource@sumologic.com'
 issues_url 'https://github.com/SumoLogic/sumologic-collector-chef-cookbook/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/SumoLogic/sumologic-collector-chef-cookbook' if respond_to?(:source_url)
-license 'Apache v2.0'
+license 'Apache-2.0'
 description 'Installs/Configures sumologic-collector'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.2.20'
