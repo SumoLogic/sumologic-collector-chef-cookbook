f9b65833fdafe7f7f1471a359f20d0aa2eea7809
Ben Abrams <me@benabrams.it>
2022-01-24 02:22:23 -0800

    allow the ability to cleanup previously configured local sources
    In some cases even if temporarily one may want to remove a source from being ingested into sumologic. In other cases it's cleaning up dead code for something that no longer generates logs in the environment.
    
    Signed-off-by: Ben Abrams <me@benabrams.it>

--- libraries/resource_source.rb
+++ libraries/resource_source.rb
@@ -10,7 +10,7 @@ class Chef
 
       default_action :create
 
-      actions :create
+      actions :create, :remove
 
       attribute :owner, regex: Chef::Config[:user_valid_regex]
       attribute :group, regex: Chef::Config[:group_valid_regex]
