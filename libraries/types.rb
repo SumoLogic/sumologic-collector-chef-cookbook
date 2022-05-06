c066ec1def7b4319709196cf2549eb80c1c6bb10
Dominik Rosiek <drosiek@sumologic.com>
2022-05-06 08:03:54 +0200

    refactor: makes lint happy
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- libraries/types.rb
+++ libraries/types.rb
@@ -3,12 +3,12 @@
 EVENT_FORMAT = {
   nil => nil,
   :legacy => 0,
-  :json => 1,
-}
+  :json => 1
+}.freeze
 
 EVENT_MESSAGE = {
   nil => nil,
   :complete => 0,
   :message => 1,
-  :metadata => 2,
-}
+  :metadata => 2
+}.freeze
