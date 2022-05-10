aad1c669970076bf48df3097e891e57ae891b8dc
Dominik Rosiek <drosiek@sumologic.com>
2022-05-10 18:42:21 +0200

    tests: fix
    Signed-off-by: Dominik Rosiek <drosiek@sumologic.com>

--- test/fixtures/cookbooks/source-resource/recipes/remote_win_event_log_create.rb
+++ test/fixtures/cookbooks/source-resource/recipes/remote_win_event_log_create.rb
@@ -7,4 +7,9 @@ sumo_source_remote_windows_event_log 'remote_win_event_log' do
   password 'password'
   hosts ['myremotehost1']
   log_names %w[security application]
+
+  event_format :legacy
+  event_message :message
+  allowlist "el1,el2"
+  denylist "el3,el4"
 end
