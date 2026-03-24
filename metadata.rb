f83a3217c552d06e59d52c2859b1c6626e84b56d
Astitva Sood <astitva.sood@sumologic.com>
2026-03-24 10:43:41 +0530

    chore: fix md files, increased min chef version to 12

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,7 @@ maintainer 'Sumo Logic'
 maintainer_email 'collection@sumologic.com'
 issues_url 'https://github.com/SumoLogic/sumologic-collector-chef-cookbook/issues'
 source_url 'https://github.com/SumoLogic/sumologic-collector-chef-cookbook'
+license 'Apache-2.0'
 description 'Installs/Configures sumologic-collector'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.9.1'
