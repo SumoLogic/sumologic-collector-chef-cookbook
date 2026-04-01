45f4e023ab191d418010677a66d146d44d4f4642
Astitva Sood <astitva.sood@sumologic.com>
2026-04-01 18:57:20 +0530

    chore tests

--- test/fixtures/cookbooks/source-resource/recipes/default.rb
+++ test/fixtures/cookbooks/source-resource/recipes/default.rb
@@ -1,5 +1,12 @@
 # frozen_string_literal: true
 
+# Remove /etc/sumo.json if it exists as a file (left over from the default
+# suite running on the same host) before creating it as a directory.
+file node['sumologic']['sumo_json_path'] do
+  action :delete
+  only_if { ::File.file?(node['sumologic']['sumo_json_path']) }
+end
+
 directory node['sumologic']['sumo_json_path'] do
   action :create
   recursive true
